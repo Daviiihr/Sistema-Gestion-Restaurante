e66d6cf69609d7ff5d5469eedbdb1e34f4c47ca6
LizardoFSA <lsalazar2021@alu.uct.cl>
2025-10-11 23:04:49 -0300

    feat: Actualizar catálogo de menús y lógica de boleta

--- ElementoMenu.py
+++ ElementoMenu.py
@@ -10,17 +10,16 @@ class CrearMenu(IMenu):
     ingredientes: List[Ingrediente]
     precio: float = 0.0
     icono_path: Optional[str] = None
-    cantidad: int = field(default=0, compare=False)
+    cantidad: int = field(default=0, compare=False, repr=False)
 
     def esta_disponible(self, stock: Stock) -> bool:
         for req in self.ingredientes:
             ok = False
             for ing in stock.lista_ingredientes:
-                if ing.nombre == req.nombre and (req.unidad is None or ing.unidad == req.unidad):
-                    if int(ing.cantidad) >= int(req.cantidad):
-                        ok = True
-                        break
-            if not ok:
-                return False
-        return True
+                if ing.nombre.lower() == req.nombre.lower() and int(ing.cantidad) :
+                    ok = True
+                    break
+                if not ok:
+                    return False
+                return True
 
